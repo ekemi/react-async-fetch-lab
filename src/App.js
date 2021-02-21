52a61593804d7c863ffa53395287ee45807e0257
Yapnounkie <edmundopierre@yahoo.com>
2021-02-20 19:52:46 -0800

    fix minor error

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, {Component} from 'react'
+
+class App extends Component {
+
+    state = {
+        users: []
+    }
+
+    componentDidMount() {
+        return fetch('http://api.open-notify.org/astros.json')
+        .then(res=>res.json())
+        .then(usersData=>{
+            this.setState({
+                users: usersData.people
+
+            })
+
+        })
+
+    }
+    render() {
+        return (
+            <div>
+                {this.state.users.map(person=>person.name)}
+            </div>)
+    }
+}
+
+ export default App
